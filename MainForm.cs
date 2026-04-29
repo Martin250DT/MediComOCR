caa91725a79aa0688c94a03d9e2f3b040b8eeb2c
Martin250DT <matoxxx@gmail.com>
2026-04-29 10:35:21 +0200

    Fix WinRT reference warnings by using Windows SDK contracts package

--- MainForm.cs
+++ MainForm.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Drawing;
 using System.IO;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Windows.Graphics.Imaging;
